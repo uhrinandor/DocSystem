dafd6855960353f4012146f206dbc49a13b3e57f
Uhrin Andor <andoruhrin@gmail.com>
2026-05-06 00:00:09 +0200

    server compatible

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiRootUrl: 'http://localhost:5207',
+  apiRootUrl: 'http://187.127.79.188:5207',
   openApiSpecUrl: '/openapi/DocSystem.json',
   groqBaseUrl: 'https://api.groq.com/openai/v1',
   groqModel: 'openai/gpt-oss-20b',
